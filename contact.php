4e4b49f8993a6fdfb4a10cfb7d62e12316dabc48
Gappiyawuto <gappiyawuto@gmail.com>
2025-01-08 22:29:03 +0530

    php

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
 <?php
 
-// Only process POST reqeusts.
+// Only process POST requests.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form fields and remove whitespace.
     $name = strip_tags(trim($_POST["name"]));
-	$name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $name = str_replace(array("\r", "\n"), array(" ", " "), $name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
     // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Set a 400 (bad request) response code and exit.
         http_response_code(400);
         echo "Oops! There was a problem with your submission. Please complete the form and try again.";
@@ -17,13 +17,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Update this to your desired email address.
-    $recipient = "kevindu.np@gmail.com";
-	$subject = "Message from $name";
+    $recipient = "kevindu.np@gmail.com"; // Changed to kevindu.np@gmail.com
+    $subject = "Message from $name";
 
     // Email content.
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
-    $email_content .= "Subject: $subject\n\n";
     $email_content .= "Message: $message\n";
 
     // Email headers.
@@ -44,3 +43,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
+}
+?>
